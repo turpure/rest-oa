6a557b560ed958e8aeaf9e3c5732aeb83c2d147a
turpure <p812816627@163.com>
2018-05-10 18:33:51 +0800

    admin base controller

--- api/modules/v1/controllers/AdminController.php
+++ api/modules/v1/controllers/AdminController.php
@@ -1,14 +1,75 @@
 <?php
 /**
  * @desc PhpStorm.
- * @author: Administrator
+ * @author: turpure
  * @since: 2018-05-10 17:32
  */
 
 namespace api\modules\v1\controllers;
 
+use common\models\User;
+use yii\rest\ActiveController;
+use yii\helpers\ArrayHelper;
+use yii\filters\auth\CompositeAuth;
+use yii\filters\auth\HttpBasicAuth;
+use yii\filters\auth\HttpBearerAuth;
+use yii\filters\auth\QueryParamAuth;
+use yii\filters\Cors;
+use yii\base\UserException;
 
-class AdminController
+
+class AdminController extends ActiveController
 {
+    public function behaviors()
+    {
+        $behaviors = ArrayHelper::merge([
+            [
+                'class' => Cors::className(),
+            ]
+        ],
+            parent::behaviors()
+        );
+
+        $behaviors['authenticator'] = [
+            'class' => CompositeAuth::className(),
+
+            'authMethods' => [
+                ['class' => HttpBasicAuth::className(),'auth' => [$this, 'auth']],
+                ['class' => HttpBearerAuth::className()],
+                ['class' => QueryParamAuth::className(),'tokenParam' => 'token',],
+
+            ],
+            'optional' =>[
+                'login',
+                'signup'
+            ]
+        ];
+        return $behaviors;
+    }
+
+    public function actions()
+    {
+        $actions =  parent::actions();
+        return $actions;
+    }
+
+    /*
+     * basic-auth auth
+     */
+    public function auth($username, $password)
+    {
+        $user = User::findByUsername($username);
+        if(empty($username) || empty($password) || empty($user)) {
+            //return false;
+            //OR
+            throw new UserException("There is an error!");
+        }
+        if ($user->validatePassword($password)) {
+            return $user;
+        }
+        //return false;
+        //OR
+        throw new UserException("Wrong username or password!");
+    }
 
 }
--- api/modules/v1/controllers/UserController.php
+++ api/modules/v1/controllers/UserController.php
@@ -1,82 +1,17 @@
 <?php
 
 namespace api\modules\v1\controllers;
-use yii\rest\ActiveController;
-use yii\helpers\ArrayHelper;
-use yii\filters\auth\CompositeAuth;
-use yii\filters\auth\HttpBasicAuth;
-use yii\filters\auth\HttpBearerAuth;
-use yii\filters\auth\QueryParamAuth;
-use common\models\User;
+
 use api\models\LoginForm;
 use api\models\SignupForm;
 use Yii;
-use yii\filters\Cors;
-use yii\base\UserException;
+use api\modules\v1\controllers\AdminController;
 
-class UserController extends ActiveController
+class UserController extends AdminController
 {
     public $modelClass = 'api\models\User';
 
-    public function behaviors()
-        {
-            $behaviors = ArrayHelper::merge([
-                [
-                    'class' => Cors::className(),
-                ]
-            ],
-                parent::behaviors()
-            );
 
-            $behaviors['authenticator'] = [
-                'class' => CompositeAuth::className(),
-
-                'authMethods' => [
-                    ['class' => HttpBasicAuth::className(),'auth' => [$this, 'auth']],
-                    ['class' => HttpBearerAuth::className()],
-                    ['class' => QueryParamAuth::className(),'tokenParam' => 'token',],
-
-                ],
-                'optional' =>[
-                    'login',
-                    'signup'
-                ]
-            ];
-            return $behaviors;
-        }
-
-    public function actions()
-    {
-        $actions =  parent::actions();
-        return $actions;
-    }
-    /**
-     * sing-up-test
-     */
-    /*
-    public function actionSignupTest ()
-    {
-        $request = Yii::$app->request;
-        if(!$request->isPost)
-        {
-            return [
-                'code' => '404'
-            ];
-        }
-        $post = $request->post();
-        $user = new User();
-        $user->generateAuthKey();
-        $user->generateApiToken();
-        $user->setPassword($post['password']);
-        $user->username = $post['username'];
-        $user->email = $post['email'];
-        $user->save(false);
-
-        return [
-            'code' => 0
-        ];
-    }
-*/
     /**
      * sing up
      */
@@ -141,19 +76,5 @@ class UserController extends ActiveController
         ];
     }
 
-    public function auth($username, $password)
-    {
-        $user = User::findByUsername($username);
-        if(empty($username) || empty($password) || empty($user)) {
-            //return false;
-            //OR
-            throw new UserException("There is an error!");
-        }
-        if ($user->validatePassword($password)) {
-            return $user;
-        }
-        //return false;
-        //OR
-        throw new UserException("Wrong username or password!");
-    }
+
 }
