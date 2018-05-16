cff775807e96ccc3f9db7582dba0a91228554ae3
turpure <p812816627@163.com>
2018-05-16 14:28:15 +0800

    check access of goods controller

--- api/config/main.php
+++ api/config/main.php
@@ -17,37 +17,6 @@ return [
         'rbac' => [
             'class' => 'wind\rest\modules'
         ],
-        'oauth2' => [
-            'class' => 'filsh\yii2\oauth2server\Module',
-            'components' => [
-                'request' => function () {
-                    return \filsh\yii2\oauth2server\Request::createFromGlobals();
-                },
-                'response' => [
-                    'class' => \filsh\yii2\oauth2server\Response::className(),
-                ],
-            ],
-            'tokenParamName' => 'token',
-            'tokenAccessLifetime' => 3600 * 24,
-            'storageMap' => [
-                'user_credentials' => 'common\models\Auth',
-            ],
-            'grantTypes' => [
-                'user_credentials' => [
-                    'class' => 'OAuth2\GrantType\UserCredentials',
-                ],
-                'client_credentials' => [
-                    'class' => 'OAuth2\GrantType\ClientCredentials',
-                ],
-                'refresh_token' => [
-                    'class' => 'OAuth2\GrantType\RefreshToken',
-                    'always_issue_new_refresh_token' => true
-                ],
-                'authorization_code' => [
-                    'class' => 'OAuth2\GrantType\AuthorizationCode'
-                ],
-            ]
-        ],
     ],
 
     'components' => [
@@ -62,15 +31,15 @@ return [
         ],
         'response' => [
             'class' => 'yii\web\Response',
-            'on beforeSend' => function ($event) {
-                $response = $event->sender;
-                $response->data = [
-                    'code' => $response->getStatusCode(),
-                    'data' => $response->data,
-                    'message' => $response->statusText
-                ];
-                $response->format = yii\web\Response::FORMAT_JSON;
-            },
+//            'on beforeSend' => function ($event) {
+//                $response = $event->sender;
+//                $response->data = [
+//                    'code' => $response->getStatusCode(),
+//                    'data' => $response->data,
+//                    'message' => $response->statusText
+//                ];
+//                $response->format = yii\web\Response::FORMAT_JSON;
+//            },
         ],
 
     // api-token
@@ -98,110 +67,7 @@ return [
         'urlManager' => [
             'enablePrettyUrl' => true,
             'showScriptName' => false,
-//            'enableStrictParsing' =>true,
-            'rules' => [
-                'POST oauth2/<action:\w+>' => 'oauth2/rest/<action>',
-                //权限
-                [
-                    'class' => 'yii\rest\UrlRule',
-                    'controller' => ['rbac/permission'],
-                    'extraPatterns' => [
-                        'GET view' => 'view',
-                        'DELETE delete' => 'delete',
-                        'POST update' => 'update',
-                        'POST assign' => 'assign',
-                        'POST remove' => 'remove',
-                        'GET assign-list' => 'assign-list',
-                    ]
-                ],
-                //菜单
-                [
-                    'class' => 'yii\rest\UrlRule',
-                    'controller' => ['rbac/menu'],
-                    'extraPatterns' => [
-                        'GET parent' => 'parent',
-                        'POST create' => 'create',
-                        'POST update' => 'update',
-                        'GET user' => 'user-menu'
-                    ]
-                ],
-                //路由
-                [
-                    'class' => 'yii\rest\UrlRule',
-                    'controller' => ['rbac/route'],
-                    'extraPatterns' => [
-                        'POST remove' => 'remove',
-                        'GET  all' => 'all',
-                        'GET  parent' => 'parent',
-                    ]
-                ],
-                //角色
-                [
-                    'class' => 'yii\rest\UrlRule',
-                    'controller' => ['rbac/role'],
-                    'extraPatterns' => [
-                        'GET view' => 'view',
-                        'DELETE delete' => 'delete',
-                        'POST update' => 'update',
-                        'POST assign' => 'assign',
-                        'GET assign-list' => 'assign-list',
-                        'POST remove' => 'remove',
-                    ]
-                ],
-                //分配
-                [
-                    'class' => 'yii\rest\UrlRule',
-                    'controller' => ['rbac/assignment'],
-                    'extraPatterns' => [
-                        'GET view' => 'view',
-                        'POST assign' => 'assign',
-                        'POST revoke' => 'revoke',
-                        'GET assign-list' => 'assign-list',
-                        'POST remove' => 'remove',
-                        'POST assign-batch' => 'assign-batch',
-                        'POST assign-remove' => 'remove-users',
-                        'GET assign-users' => 'assign-users',
-                    ]
-                ],
-                //用户
-                [
-                    'class' => 'yii\rest\UrlRule',
-                    'controller' => ['rbac/user'],
-                    'extraPatterns' => [
-                        'GET view' => 'view',
-                        'POST activate' => 'activate',
-                    ]
-                ],
-                //规则
-                [
-                    'class' => 'yii\rest\UrlRule',
-                    'controller' => ['rbac/rule'],
-                    'extraPatterns' => [
-                        'GET index' => 'get-rules',
-                        'POST create' => 'create',
-                        'POST delete' => 'delete',
-                        'POST update' => 'update',
-                    ]
-                ],
-                //分组
-                [
-                    'class' => 'yii\rest\UrlRule',
-                    'controller' => ['rbac/groups'],
-                    'extraPatterns' => [
-                        'POST assign' => 'assign',
-                        'POST revoke' => 'revoke',
-                        'GET assign-user' => 'assign-user',
-                    ]
-                ]
-            ],
-        ]
-    ],
-    'as access' => [
-        'class' => 'wind\rest\components\AccessControl',
-        'allowActions' => [
-            'site/*',//允许访问的节点，可自行添加
-            'rbac/menu/user-menu',
-            'oauth2/*',
+
         ]
     ],
     'params' => $params,
--- api/modules/v1/controllers/GoodsController.php
+++ api/modules/v1/controllers/GoodsController.php
@@ -4,7 +4,8 @@ namespace api\modules\v1\controllers;
 
 use yii\rest\ActiveController;
 use Yii;
-class GoodsController extends ActiveController
+use api\modules\v1\controllers\AdminController;
+class GoodsController extends AdminController
 {
 
     public $modelClass = 'api\models\Goods';
@@ -13,5 +14,15 @@ class GoodsController extends ActiveController
     {
         $headers = Yii::$app->request->headers;
         return $headers;
+    }
+
+    public function checkAccess($action, $model = null, $params = [])
+    {
+        $user = $this->authenticate(Yii::$app->user, Yii::$app->request, Yii::$app->response);
+        $userId = $user?$user->getId():'';
+        if ($userId == 6)
+        {
+            throw new \yii\web\ForbiddenHttpException("No permiession!");
+        }
     }
 }
